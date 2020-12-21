a8675f2e4db6e8aac41a8245cef6c1add5f8abfb
Nicolas <ncardonab@unal.edu.co>
2020-12-21 18:08:54 -0500

    Background section done!, for both web and mobile

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./Background.css";
+import ViewMoreButton from "../ViewMoreButton/ViewMoreButton";
 
-const Background = () => {
-  return <div className="background"></div>;
-};
+function Background() {
+  return (
+    <div className="bg-img">
+      <div className="bg-container">
+        <h1 className="bg-title text-light">Background</h1>
+        <div className="bg-text-container">
+          <div className="bg-text text-light">
+            This international research group has its origin within the
+            successful experience with the previous IGCP project “Unifying
+            international research forces to unlock and strengthen geothermal
+            exploitation of the Americas and Europe” (2016-2019).
+          </div>
+          <ViewMoreButton></ViewMoreButton>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Background;
