c83d01d92702774426267c3c65fa2095c2251816
Nicolas <ncardonab@unal.edu.co>
2021-01-07 14:48:18 -0500

    Researchers section for both mobile & desktop, done. Also created Pill component and Updated Slider adding a effect whenever the window\'s size changes reset the x value

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Slider.css";
 import nextIcon from "../../assets/SVGs/Next.svg";
 import prevIcon from "../../assets/SVGs/Prev.svg";
+import darkNextIcon from "../../assets/SVGs/Dark Next.svg";
+import darkPrevIcon from "../../assets/SVGs/Dark Prev.svg";
 
-function Slider({ Component, slides }) {
+function Slider({ Component, slides, indicators = true, isDark = false }) {
   const [x, setX] = useState(0);
+
+  useEffect(() => {
+    const updateX = () => {
+      const newWidth = window.innerWidth;
+      if (newWidth > 900) {
+        setX(0);
+      }
+    };
+
+    window.addEventListener("resize", updateX);
+  }, []);
+
   const goRight = () => {
     x === -100 * (slides.length - 1) ? setX(0) : setX(x - 100);
   };
@@ -14,7 +28,9 @@ function Slider({ Component, slides }) {
   };
 
   return (
-    <div className="Slider">
+    <div
+      className={`Slider ${isDark ? "dark" : ""} ${indicators ? "active" : ""}`}
+    >
       {slides.map((slide, index) => {
         return (
           <div
@@ -27,23 +43,27 @@ function Slider({ Component, slides }) {
         );
       })}
       <button className="goLeft" onClick={goLeft}>
-        <img src={prevIcon} alt="left icon" />
+        <img src={isDark ? darkPrevIcon : prevIcon} alt="left icon" />
       </button>
       <button className="goRight" onClick={goRight}>
-        <img src={nextIcon} alt="right icon" />
+        <img src={isDark ? darkNextIcon : nextIcon} alt="right icon" />
       </button>
-      <div className="slider-indicators">
-        {slides.map((info, index) => {
-          return (
-            <div
-              key={index}
-              className={`slider-indicator ${
-                index === -(x / 100) ? "active" : ""
-              }`}
-            ></div>
-          );
-        })}
-      </div>
+      {indicators ? (
+        <div className="slider-indicators">
+          {slides.map((info, index) => {
+            return (
+              <div
+                key={index}
+                className={`slider-indicator ${
+                  index === -(x / 100) ? "active" : ""
+                }`}
+              ></div>
+            );
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
