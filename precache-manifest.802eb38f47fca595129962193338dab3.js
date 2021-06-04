f39d206402bc983afe63a85b0ef4bdd7c8c206db
Nicolas Cardona <ncardonab@unal.edu.co>
2021-06-04 15:03:20 -0500

    Updates

---
+++ precache-manifest.802eb38f47fca595129962193338dab3.js
@@ -0,0 +1,542 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0240a18bc60cddb4c096ec75a3851e22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33f9a3cbf7bc0c75772f",
+    "url": "/static/css/2.fa349ff9.chunk.css"
+  },
+  {
+    "revision": "6f899f06ac6b766fe7cb",
+    "url": "/static/css/main.75971172.chunk.css"
+  },
+  {
+    "revision": "33f9a3cbf7bc0c75772f",
+    "url": "/static/js/2.aea8a87c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.aea8a87c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f899f06ac6b766fe7cb",
+    "url": "/static/js/main.2a22c7d8.chunk.js"
+  },
+  {
+    "revision": "3701a324fe23b0e77728",
+    "url": "/static/js/runtime-main.5f51c4e4.js"
+  },
+  {
+    "revision": "4d2f1b00277fa7e3bb739e768544e1b8",
+    "url": "/static/media/AGEOCOL.4d2f1b00.png"
+  },
+  {
+    "revision": "efe02ac3d5aee66534ea3c8ede933ede",
+    "url": "/static/media/Abdelhakim Djebara Argelia.efe02ac3.png"
+  },
+  {
+    "revision": "fae0b606f8a470c457c9af48d310e2bb",
+    "url": "/static/media/Abdelkader Filali Argelia.fae0b606.png"
+  },
+  {
+    "revision": "41dd376e1bf4b9babc484f0e1a3d2a0b",
+    "url": "/static/media/Aim background.41dd376e.png"
+  },
+  {
+    "revision": "c7348151c963fe4c9cd7b6411ebaafea",
+    "url": "/static/media/Algeria CDER.c7348151.jpg"
+  },
+  {
+    "revision": "7925b1a10102b3938aa7cf815f61a06a",
+    "url": "/static/media/Argelia Ecole National Polytecnique Constantine.7925b1a1.jpg"
+  },
+  {
+    "revision": "bdd0975abb724ce84e0f31ae1a633dd5",
+    "url": "/static/media/Argentina.bdd0975a.png"
+  },
+  {
+    "revision": "165f1f447b8fb9e859c00f70e3fff4ca",
+    "url": "/static/media/BRGM background.165f1f44.png"
+  },
+  {
+    "revision": "a9abcee19840a0b8a03a8536ba6211c7",
+    "url": "/static/media/Brown Soil Blurred.a9abcee1.jpg"
+  },
+  {
+    "revision": "2f16d0400b94d1ba6a269152dc34cc00",
+    "url": "/static/media/Campus SGC.2f16d040.jpg"
+  },
+  {
+    "revision": "58b79cf4ba1ace052b678c50e9a0845a",
+    "url": "/static/media/Carlo Janna Italy.58b79cf4.png"
+  },
+  {
+    "revision": "b579e3525fa94fb98825718834de5ec0",
+    "url": "/static/media/Cascada.b579e352.png"
+  },
+  {
+    "revision": "520ebf1ed1dccd99c196a67e66c035bf",
+    "url": "/static/media/Checklist.520ebf1e.svg"
+  },
+  {
+    "revision": "65c84ffe1fe91d9b4a194da672c2c2da",
+    "url": "/static/media/Claudia Alfaro Colombia.65c84ffe.png"
+  },
+  {
+    "revision": "9c74d715c2902386e16ab502e50ae070",
+    "url": "/static/media/Claudia Zoccarato Italy.9c74d715.png"
+  },
+  {
+    "revision": "19ab9b365cc8bbc2492f73657fb14538",
+    "url": "/static/media/Colombia AGEOCOL.19ab9b36.jpg"
+  },
+  {
+    "revision": "aa0f04cb56d0fae904da83c7d6666b80",
+    "url": "/static/media/Colombia UdeM.aa0f04cb.jpg"
+  },
+  {
+    "revision": "9253da9377d694f4a080cb5d4ec9937e",
+    "url": "/static/media/Daniela Blessent Colombia.9253da93.png"
+  },
+  {
+    "revision": "8670b7cb41f808701bec1c28e1f4d8af",
+    "url": "/static/media/Dark Next.8670b7cb.svg"
+  },
+  {
+    "revision": "77abec9f5556dd4c0cd6d8ec83813684",
+    "url": "/static/media/Dark Prev.77abec9f.svg"
+  },
+  {
+    "revision": "395b82a4346e31fcfa60ac6e628b58a0",
+    "url": "/static/media/Edificio Rectoría IBERO.395b82a4.jpg"
+  },
+  {
+    "revision": "947f3291f67fbfb19b68e232d11ad57e",
+    "url": "/static/media/Escudo_Universidad_de_Medellin.947f3291.svg"
+  },
+  {
+    "revision": "acfe7fad5325fb9c2b174c09dbd590b6",
+    "url": "/static/media/Fernando Rivas Mexico.acfe7fad.png"
+  },
+  {
+    "revision": "f0fd0aa9253223ce2f48a63f9e9a8b30",
+    "url": "/static/media/France Rennes.f0fd0aa9.jpg"
+  },
+  {
+    "revision": "dd76557836359e66b317cd4de892df76",
+    "url": "/static/media/Gallery Background Image.dd765578.png"
+  },
+  {
+    "revision": "60d804d48a2a50b66c44c2be818cb29c",
+    "url": "/static/media/GeoZS.60d804d4.jpg"
+  },
+  {
+    "revision": "3668aeda2f2408a93a9c33583b2a100d",
+    "url": "/static/media/Hamza Semmari Argelia.3668aeda.png"
+  },
+  {
+    "revision": "bfdc708f84e92633133d783169bcc6f0",
+    "url": "/static/media/Hejuan Liu China.bfdc708f.png"
+  },
+  {
+    "revision": "644852343be7422ba9d22665fb8c4327",
+    "url": "/static/media/ICCG Cataluña España.64485234.jpg"
+  },
+  {
+    "revision": "69ff5fc9e1b9ab105a22d1f2fb40508e",
+    "url": "/static/media/INEEL mexico.69ff5fc9.jpg"
+  },
+  {
+    "revision": "eaf3604d17c76bc691252a5c041c7c18",
+    "url": "/static/media/INGV italia Napoli.eaf3604d.jpg"
+  },
+  {
+    "revision": "64c453eb8d6bdab0acbc565be2384959",
+    "url": "/static/media/INRS imagen.64c453eb.jpg"
+  },
+  {
+    "revision": "bfcf4bf09cc0c9fa8fdba246feda6fde",
+    "url": "/static/media/IUGS icon.bfcf4bf0.png"
+  },
+  {
+    "revision": "f82f0b1efc3b2cdbaf7e6b9d721c3d17",
+    "url": "/static/media/Iceland ISE.f82f0b1e.jpeg"
+  },
+  {
+    "revision": "99d525d453e18fc2a597a8449027ae91",
+    "url": "/static/media/Ignasi Herms Spain.99d525d4.png"
+  },
+  {
+    "revision": "e4e6f481a0d0fc3fb3f24b3305941218",
+    "url": "/static/media/Jacqueline López Colombia.e4e6f481.png"
+  },
+  {
+    "revision": "9aeb93c4a06aaa362ce8a1591437d3ff",
+    "url": "/static/media/Jasmin Raymond Canada.9aeb93c4.png"
+  },
+  {
+    "revision": "52eedb02ceff11c4f55f46e81b70f7cd",
+    "url": "/static/media/Jose Acuña Sweden.52eedb02.png"
+  },
+  {
+    "revision": "defe50a9054a52741a77ebd7d805c606",
+    "url": "/static/media/KTH Suecia.defe50a9.png"
+  },
+  {
+    "revision": "b5ea4d24d55ea1a4df51f83b75e69104",
+    "url": "/static/media/Krafla Geothermal Power Station blurred.b5ea4d24.png"
+  },
+  {
+    "revision": "ce86fa555ed9d8526b0b59ce67757b8e",
+    "url": "/static/media/Linda Daniele Chile.ce86fa55.png"
+  },
+  {
+    "revision": "0f69dd6821f5eb49b19189ade5a766ce",
+    "url": "/static/media/LinkedIn Logo.0f69dd68.svg"
+  },
+  {
+    "revision": "1803405c476df3133b7be181285c57d6",
+    "url": "/static/media/Logo EOS.1803405c.png"
+  },
+  {
+    "revision": "5588f24d92acc95f349b46e768bd6952",
+    "url": "/static/media/Ludmila Adam New Zealand.5588f24d.png"
+  },
+  {
+    "revision": "d01098f328aff073100af26105547d81",
+    "url": "/static/media/Luka Serianz Slovenia.d01098f3.png"
+  },
+  {
+    "revision": "7af1da22cd8475edcbe3c5063b650b0b",
+    "url": "/static/media/Madelaine Constance United Kingdom.7af1da22.png"
+  },
+  {
+    "revision": "234d748333a1f55e6f879b2b2e1a84f2",
+    "url": "/static/media/Mar Alcaraz Argentina.234d7483.png"
+  },
+  {
+    "revision": "4bfe4360bb7b42e2f283eb3d6b6d416a",
+    "url": "/static/media/Mexico.4bfe4360.png"
+  },
+  {
+    "revision": "893ae389805678f9e439f42dff1b1e53",
+    "url": "/static/media/Ministerio El Salvador.893ae389.jpg"
+  },
+  {
+    "revision": "93254c78ef28e2e4d16196a429fbb232",
+    "url": "/static/media/Ministerio de Medio Ambiente.93254c78.png"
+  },
+  {
+    "revision": "5d9c7cf9ea2de08b6053d9bb9327c1bf",
+    "url": "/static/media/Mohammed Moundji Argelia.5d9c7cf9.png"
+  },
+  {
+    "revision": "4dc2a3009a0bc3851ea0f4c7b79d791f",
+    "url": "/static/media/Next.4dc2a300.svg"
+  },
+  {
+    "revision": "99955f2e5403807f423212fcb3d032a9",
+    "url": "/static/media/Nina Rman Slovenia.99955f2e.png"
+  },
+  {
+    "revision": "2ad554e3eabdca6ad474e0974d17ce6a",
+    "url": "/static/media/Noureddine Benaouda Argelia.2ad554e3.png"
+  },
+  {
+    "revision": "796f7d4299090098a4f355512852f5d2",
+    "url": "/static/media/Oscar Llamosa Australia.796f7d42.png"
+  },
+  {
+    "revision": "f623eea1d2d201c60e8578dfda588d69",
+    "url": "/static/media/Pablo Aguilera New Zealand.f623eea1.png"
+  },
+  {
+    "revision": "9b49630a3abf64bec39937e378e3a18f",
+    "url": "/static/media/Padova Italy.9b49630a.jpg"
+  },
+  {
+    "revision": "4b3dac14ee31e143f1ef571353539458",
+    "url": "/static/media/Pietro Teatini Italy.4b3dac14.png"
+  },
+  {
+    "revision": "81ef1fad7729d36b43020d7c782d1a92",
+    "url": "/static/media/Prev.81ef1fad.svg"
+  },
+  {
+    "revision": "c43e7bfc373f044bf239f39c0dc58ce6",
+    "url": "/static/media/RIGS.c43e7bfc.jpg"
+  },
+  {
+    "revision": "6d60e05a00b113f844f2f8dd12fc0f91",
+    "url": "/static/media/Renato Somma Italy.6d60e05a.jpg"
+  },
+  {
+    "revision": "d4169907954565df65ce88c7bed472f5",
+    "url": "/static/media/Rene Therrien Canada.d4169907.png"
+  },
+  {
+    "revision": "59c2166765f40a7b0dfe042f481f1543",
+    "url": "/static/media/Saeid Mohammadzadeh Japan.59c21667.png"
+  },
+  {
+    "revision": "7465b516b7577487916704755766d5d6",
+    "url": "/static/media/Salima Ouali Argelia.7465b516.png"
+  },
+  {
+    "revision": "49776ec9e6bc89be9c627b8cf933709c",
+    "url": "/static/media/Salvador.49776ec9.png"
+  },
+  {
+    "revision": "b71ff58a5087d179129e1edf9556df65",
+    "url": "/static/media/Servicio Geológico Colombiano.b71ff58a.jpg"
+  },
+  {
+    "revision": "b0c8836ef5a92e9426ac0b9bd060e955",
+    "url": "/static/media/Slovenia GeoZS2.b0c8836e.jpg"
+  },
+  {
+    "revision": "5f4076babd8cd531c22d4de988d07edf",
+    "url": "/static/media/Sofiane Aberkane Argelia.5f4076ba.png"
+  },
+  {
+    "revision": "953815de82a27844785e84251dc2c14a",
+    "url": "/static/media/Spain.953815de.png"
+  },
+  {
+    "revision": "5b7844ce752162f6a6c2a8af7202ab2c",
+    "url": "/static/media/Tree icon.5b7844ce.svg"
+  },
+  {
+    "revision": "e769ad5b597b59645806e34ab354014d",
+    "url": "/static/media/UMons.e769ad5b.jpg"
+  },
+  {
+    "revision": "2ba09e54ca6e01d00d40cf837fb04703",
+    "url": "/static/media/Univ Laval Canada.2ba09e54.jpeg"
+  },
+  {
+    "revision": "9708bc50a355b376cea40681db24e0fa",
+    "url": "/static/media/University of Auckland Background.9708bc50.jpg"
+  },
+  {
+    "revision": "384f142b2bba5cfd6779c33a6830349c",
+    "url": "/static/media/University of Auckland.384f142b.png"
+  },
+  {
+    "revision": "d1cd3c7b2389082cbd1d1a7bbd524426",
+    "url": "/static/media/Urge Image.d1cd3c7b.png"
+  },
+  {
+    "revision": "3c180f727696b04c304c9e48732620ec",
+    "url": "/static/media/Vesuvio.3c180f72.jpg"
+  },
+  {
+    "revision": "112121165b07da983c7743ed90044e68",
+    "url": "/static/media/View More Icon 30x30.11212116.svg"
+  },
+  {
+    "revision": "5b172b4293bf4f916fd32e194cea8585",
+    "url": "/static/media/Villarrica Volcano Blurred.5b172b42.png"
+  },
+  {
+    "revision": "bf4f556bd1e9e30dd1fa110e82ffa9bd",
+    "url": "/static/media/Women in geothermal Energy.bf4f556b.png"
+  },
+  {
+    "revision": "82bf59795b876f920bf00b6fdc4d897d",
+    "url": "/static/media/adventure-calm-clouds-dawn-414171.82bf5979.jpg"
+  },
+  {
+    "revision": "46db4c808f85e01cd6f421ad674aa2a6",
+    "url": "/static/media/akita Japon.46db4c80.jpg"
+  },
+  {
+    "revision": "278f93ad193e3301b8b0804e30221507",
+    "url": "/static/media/brgm.278f93ad.png"
+  },
+  {
+    "revision": "32354ffa76c18c3374269298840e690a",
+    "url": "/static/media/cder.32354ffa.jpg"
+  },
+  {
+    "revision": "d493d2b873c8733de480e89e8b4efe69",
+    "url": "/static/media/cega.d493d2b8.png"
+  },
+  {
+    "revision": "5c346d226ac5209010342d920b46e19e",
+    "url": "/static/media/energia-geotermica-geiser2-600x399.5c346d22.jpg"
+  },
+  {
+    "revision": "5682e610831e7e45c563c9e1044c28bc",
+    "url": "/static/media/enpc.5682e610.png"
+  },
+  {
+    "revision": "763a8af9200f9b4719559a6460910a9b",
+    "url": "/static/media/facebook-f-brands.763a8af9.svg"
+  },
+  {
+    "revision": "63790b42b3ca432b03efb6ccc05201f2",
+    "url": "/static/media/fondo CEGA Chile.63790b42.jpg"
+  },
+  {
+    "revision": "2eadf5850d88cf6e747e8b37ae44ab0e",
+    "url": "/static/media/fondo UPTC España.2eadf585.jpg"
+  },
+  {
+    "revision": "0b6d5d3e1b7b7feaf250ec0333511aae",
+    "url": "/static/media/fondo empresa EOS.0b6d5d3e.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fb240aa83d57a44191a4dd0ad0f18ab8",
+    "url": "/static/media/gmail.fb240aa8.svg"
+  },
+  {
+    "revision": "d1fe53aac35e51e931e4bbd8eb3cd237",
+    "url": "/static/media/grand-prismatics-bacterial-color-3864527_1920.d1fe53aa.jpg"
+  },
+  {
+    "revision": "c55e9ecd403c74bb2dce5f249efd22b5",
+    "url": "/static/media/group.c55e9ecd.svg"
+  },
+  {
+    "revision": "2e200b0a852cb590c85ab1f49e499b62",
+    "url": "/static/media/hr.2e200b0a.jpg"
+  },
+  {
+    "revision": "95e2e89a104b781f6305a2dee86f95aa",
+    "url": "/static/media/icgc.95e2e89a.png"
+  },
+  {
+    "revision": "9a24b7c2ee455f0583e6cb793e09217e",
+    "url": "/static/media/icons8-ophthalmology-100.9a24b7c2.svg"
+  },
+  {
+    "revision": "d11b6275f6bf831cfd3adf2159b7e7d6",
+    "url": "/static/media/icons8-tree-structure-100.d11b6275.svg"
+  },
+  {
+    "revision": "dfdb75c6f89ba0220ee8c9e4aabbd192",
+    "url": "/static/media/ihlla.dfdb75c6.png"
+  },
+  {
+    "revision": "25a96a0e890032037b2560d2a1257aec",
+    "url": "/static/media/ineel.25a96a0e.png"
+  },
+  {
+    "revision": "826d654793e9089e5c8bfd9fb2071de0",
+    "url": "/static/media/ingv.826d6547.png"
+  },
+  {
+    "revision": "9b31b74d4d127d21ee4d741bc30101f9",
+    "url": "/static/media/inrs icon.9b31b74d.png"
+  },
+  {
+    "revision": "6d4ea46aa9ac85d44527b0c43a336022",
+    "url": "/static/media/irsm.6d4ea46a.svg"
+  },
+  {
+    "revision": "6ea7728fd18a222224a43701615ac7bd",
+    "url": "/static/media/kth.6ea7728f.png"
+  },
+  {
+    "revision": "9337e5c054eb6875fe9576e49bfc7bd3",
+    "url": "/static/media/laval.9337e5c0.png"
+  },
+  {
+    "revision": "ce21f45280de3869819ea7cddc7f99a8",
+    "url": "/static/media/logo - IBERO.ce21f452.png"
+  },
+  {
+    "revision": "06b9e7f3d72df2561dd0353d0bf26b83",
+    "url": "/static/media/logo UPTC España.06b9e7f3.png"
+  },
+  {
+    "revision": "8a61fc3031d42496da015aa6a11ed179",
+    "url": "/static/media/maintenance.8a61fc30.svg"
+  },
+  {
+    "revision": "8473c4ea703d578b66e8e4d908524de9",
+    "url": "/static/media/maps-and-flags.8473c4ea.svg"
+  },
+  {
+    "revision": "80b2b8cd5946fb3d9bc8be100f942da5",
+    "url": "/static/media/mountain.80b2b8cd.svg"
+  },
+  {
+    "revision": "5c720e41019b61f524d6004adac29c60",
+    "url": "/static/media/nature-3437545_1920.5c720e41.jpg"
+  },
+  {
+    "revision": "12795bda003cd08f4dac0ae555005b80",
+    "url": "/static/media/padova.12795bda.png"
+  },
+  {
+    "revision": "f67606ab41408eacc8ddcbf5dba82d7b",
+    "url": "/static/media/pin-gray.f67606ab.svg"
+  },
+  {
+    "revision": "212d4b1ca9f829b15d31680f47cca625",
+    "url": "/static/media/rennes.212d4b1c.png"
+  },
+  {
+    "revision": "4789840fffd56250945bba90c82067f6",
+    "url": "/static/media/school.4789840f.svg"
+  },
+  {
+    "revision": "4b75052a99a89e522a3a524554fe4c40",
+    "url": "/static/media/target.4b75052a.svg"
+  },
+  {
+    "revision": "7a2dd852c8079a76562358571c0e235d",
+    "url": "/static/media/thermal-spring-63080_960_720.7a2dd852.jpg"
+  },
+  {
+    "revision": "752fe655bc434abb3be549bb279c34f4",
+    "url": "/static/media/twitter-brands.752fe655.svg"
+  },
+  {
+    "revision": "0ded9bfbf61f53e85fb986aebd56bc70",
+    "url": "/static/media/udm.0ded9bfb.png"
+  },
+  {
+    "revision": "b2778a3c1f282247736d3fa45fa3c486",
+    "url": "/static/media/umons.b2778a3c.png"
+  },
+  {
+    "revision": "5cefeb280786e96b6eae4f3c2c01f760",
+    "url": "/static/media/video-camera-gray.5cefeb28.svg"
+  },
+  {
+    "revision": "2de1b05037f48cff2f794f8815e9a96d",
+    "url": "/static/media/webinar1.2de1b050.jpg"
+  },
+  {
+    "revision": "0842acc83600ac188a5376f29fe4b5db",
+    "url": "/static/media/webinar2.0842acc8.jpg"
+  },
+  {
+    "revision": "bf083e8b8bc6b97a8d0f3975c3ef7c86",
+    "url": "/static/media/wing.bf083e8b.png"
+  }
+]);
